34015c83d9578cbc46abd98cf7fb700d52dc05ce
Danil Khalimov <khalimov480@gmail.com>
2024-11-22 18:21:34 +0300

    add graph auto-creation

--- main.py
+++ main.py
@@ -1,21 +1,27 @@
+import random
 import sys
-from PyQt6 import QtWidgets
 
-from main_window import Ui_MainWindow
-from input_window import Ui_DialogAdd
 import matplotlib.pyplot as plt
-from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
 import networkx as nx
+from PyQt6 import QtWidgets
+from PyQt6.QtWidgets import QMessageBox
+from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
+
+from input_window import Ui_DialogAdd
+from main_window import Ui_MainWindow
 
 
 class GraphWidget(FigureCanvas):
+    """Граф"""
+
     def __init__(self, parent=None):
         fig = plt.figure(figsize=(5, 5))
         super(GraphWidget, self).__init__(fig)
         self.ax = fig.add_subplot(111)
         self.setParent(parent)
 
-    def plot(self, graph):
+    def plot(self, graph: nx.Graph):
+        """plot(graph) отображает граф graph"""
         self.ax.clear()
         pos = nx.circular_layout(graph)
         nx.draw(graph, pos, ax=self.ax, with_labels=True, node_color='skyblue', node_size=800, font_size=10,
@@ -23,12 +29,49 @@ class GraphWidget(FigureCanvas):
         self.draw()
 
 
+def error(message: str):
+    """error(message) выводит сообщение message в появляющемся окне"""
+    msgBox = QMessageBox()
+    msgBox.setWindowTitle("Внимание!")
+    message = message.split(':')[1][1:].capitalize()
+    msgBox.setText(message)
+    msgBox.exec()
+
+
+def is_number(n: int):
+    """is_number(n) Проверяет, является ли n числом"""
+    if len(n) == 0 or (n[0] == '-' and any(x.isdigit() is False for x in n[1:])
+                       or (n[0] != '-' and any(x.isdigit() is False for x in n))):
+        return False
+    return True
+
+
+def setup_table(table: QtWidgets.QTableWidget, columns: int, rows: int, list_headers: list):
+    """setup_table(table, columns, rows, list_headers) инициализирует таблицу table
+        с columns стоблцами и rows рядами, а также с заголовками столбцов list_headers
+    """
+    table.setEnabled(True)
+    table.setRowCount(0)
+    table.setColumnCount(columns)
+    table.setRowCount(rows)
+    table.setHorizontalHeaderLabels(list_headers)
+    for i in range(columns):
+        (table.horizontalHeader().
+         setSectionResizeMode(i, QtWidgets.QHeaderView.ResizeMode.ResizeToContents))
+    table.horizontalHeader().setStretchLastSection(True)
+
+
 class InputDialog(QtWidgets.QDialog, Ui_DialogAdd):
+    """Окно ввода входных данных"""
     def __init__(self):
         super(InputDialog, self).__init__()
         self.setupUi(self)
         self.createButton.clicked.connect(self.manualCreate)
 
+        self.points = []
+        self.edges = []
+
+        # реализация Добавления, удаления и копирования в таблицах для входных данных
         self.addButtonLoads.clicked.connect(self.add_load)
         self.deleteButtonLoads.clicked.connect(self.rm_load)
         self.copyButtonLoads.clicked.connect(self.cp_load)
@@ -46,62 +89,31 @@ class InputDialog(QtWidgets.QDialog, Ui_DialogAdd):
         self.copyButtonRouters.clicked.connect(self.cp_router)
 
     def manualCreate(self):
+        """Ручное добавление данных"""
         n = self.inputNumOfPoints.text()
-        if sum(n.count(x) for x in '0123456789') != len(n) or len(n) == 0:
-            print("[manualCreate]: not a number")
+        if not is_number(n):
+            # print("[manualCreate]: введёно не число")
+            error("[manualCreate]: введёно не число")
             return
         n = int(n)
         if n <= 0:
-            print("[manualCreate]: number of nodes can't be negative or zero")
+            # print("[manualCreate]: число узлов не может быть меньше или равно нулю!")
+            error("[manualCreate]: число узлов не может быть меньше или равно нулю!")
             return
         if self.checkAuto.isChecked():
-            self.autoCreate()
+            self.autoCreate(n)
             return
 
-        self.tablePointInput.setEnabled(True)
-        self.tablePointInput.setColumnCount(3)
-        self.tablePointInput.setRowCount(n)
-        self.tablePointInput.setHorizontalHeaderLabels(["Имя узла", "X", "Y"])
-        for i in range(3):
-            (self.tablePointInput.horizontalHeader().
-             setSectionResizeMode(i, QtWidgets.QHeaderView.ResizeMode.ResizeToContents))
-        self.tablePointInput.horizontalHeader().setStretchLastSection(True)
-
-        self.tableLoads.setEnabled(True)
-        self.tableLoads.setColumnCount(3)
-        self.tableLoads.setRowCount(1)
-        self.tableLoads.setHorizontalHeaderLabels(["Из узла", "В узел", "Объём информации(в Бит/c)"])
-        for i in range(3):
-            (self.tableLoads.horizontalHeader().
-             setSectionResizeMode(i, QtWidgets.QHeaderView.ResizeMode.ResizeToContents))
-        self.tableLoads.horizontalHeader().setStretchLastSection(True)
-
-        self.tablePackagesInput.setEnabled(True)
-        self.tablePackagesInput.setColumnCount(1)
-        self.tablePackagesInput.setRowCount(1)
-        (self.tablePackagesInput.horizontalHeader().
-         setSectionResizeMode(0, QtWidgets.QHeaderView.ResizeMode.ResizeToContents))
-        self.tablePackagesInput.setHorizontalHeaderLabels(["Размер пакета(в битах)"])
-
-        self.tablePackagesInput.horizontalHeader().setStretchLastSection(True)
-
-        self.tableChannelsInput.setEnabled(True)
-        self.tableChannelsInput.setColumnCount(4)
-        self.tableChannelsInput.setRowCount(1)
-        self.tableChannelsInput.setHorizontalHeaderLabels(["Узел 1", "Узел 2", "Пропускная способность", "Стоимость"])
-        for i in range(4):
-            (self.tableChannelsInput.horizontalHeader().
-             setSectionResizeMode(i, QtWidgets.QHeaderView.ResizeMode.ResizeToContents))
-        self.tableChannelsInput.horizontalHeader().setStretchLastSection(True)
-
-        self.tableRoutersInput.setEnabled(True)
-        self.tableRoutersInput.setColumnCount(3)
-        self.tableRoutersInput.setRowCount(1)
-        self.tableRoutersInput.setHorizontalHeaderLabels(["Модель", "Пропускная способность", "Стоимость"])
-        for i in range(3):
-            (self.tableRoutersInput.horizontalHeader().
-             setSectionResizeMode(i, QtWidgets.QHeaderView.ResizeMode.ResizeToContents))
-        self.tableRoutersInput.horizontalHeader().setStretchLastSection(True)
+        setup_table(self.tablePointInput,
+                    3, n, ["Имя узла", "X", "Y"])
+        setup_table(self.tableLoads,
+                    3, 1, ["Из узла", "В узел", "Объём информации(в Бит/c)"])
+        setup_table(self.tablePackagesInput,
+                    1, 1, ["Размер пакета(в битах)"])
+        setup_table(self.tableChannelsInput,
+                    4, 1, ["Узел 1", "Узел 2", "Пропускная способность", "Стоимость"])
+        setup_table(self.tableRoutersInput,
+                    3, 1, ["Модель", "Пропускная способность", "Стоимость"])
 
     def add_load(self):
         self.tableLoads.insertRow(self.tableLoads.currentRow() + 1)
@@ -174,8 +186,45 @@ class InputDialog(QtWidgets.QDialog, Ui_DialogAdd):
                                                QtWidgets.QTableWidgetItem(
                                                    self.tableRoutersInput.item(row_count - 2, j).text()))
 
-    def autoCreate(self):
-        pass
+    def autoCreate(self, n: int):
+        setup_table(self.tablePointInput,
+                    3, n, ["Имя узла", "X", "Y"])
+        setup_table(self.tableLoads,
+                    3, 1, ["Из узла", "В узел", "Объём информации(в Байт/c)"])
+        setup_table(self.tablePackagesInput,
+                    1, 1, ["Размер пакета(в байтах)"])
+        setup_table(self.tableChannelsInput,
+                    4, 1, ["Узел 1", "Узел 2", "Пропускная способность", "Стоимость"])
+        setup_table(self.tableRoutersInput,
+                    3, 1, ["Модель", "Пропускная способность", "Стоимость"])
+
+        xs = [(random.randint(-n, n), random.randint(-n, n)) for i in range(n)]
+        # защита от повторяющихся данных
+        xs = list(set(xs))
+        while len(xs) < n:
+            to_add = (random.randint(-n, n), random.randint(-n, n))
+            if to_add not in xs:
+                xs.append((random.randint(-n, n), random.randint(-n, n)))
+
+        edges = []
+        for i in range(len(xs) - 1):
+            edges.append((i, i + 1))
+
+        price_default = "10"
+        rate_default = "256"
+        for i, (x, y) in enumerate(xs):
+            self.tablePointInput.setItem(i, 0, QtWidgets.QTableWidgetItem(str(i + 1)))
+            self.tablePointInput.setItem(i, 1, QtWidgets.QTableWidgetItem(str(x)))
+            self.tablePointInput.setItem(i, 2, QtWidgets.QTableWidgetItem(str(y)))
+        for i, (x, y) in enumerate(edges):
+            if i != 0:
+                self.tableChannelsInput.insertRow(i)
+            self.tableChannelsInput.setItem(i, 0, QtWidgets.QTableWidgetItem(str(x)))
+            self.tableChannelsInput.setItem(i, 1, QtWidgets.QTableWidgetItem(str(y)))
+            self.tableChannelsInput.setItem(i, 2, QtWidgets.QTableWidgetItem(rate_default))
+            self.tableChannelsInput.setItem(i, 3, QtWidgets.QTableWidgetItem(price_default))
+
+        self.points, self.edges = xs, edges
 
 
 class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
@@ -184,19 +233,19 @@ class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
         self.setupUi(self)
         self.plotWidget.setVisible(False)
         self.graph_widget = GraphWidget(self.plotWidget)
+        self.dialog = InputDialog()
 
         self.addChanelButton.clicked.connect(self.show_graph)
         self.inputButton.clicked.connect(self.open_input)
 
     def show_graph(self):
         G = nx.Graph()
-        G.add_edges_from([(1, 2), (1, 3), (2, 4), (3, 4), (5, 1)])
+        G.add_edges_from(self.dialog.edges)
         self.graph_widget.plot(G)
         self.plotWidget.setVisible(True)
 
     def open_input(self):
-        input_dialog = InputDialog()
-        input_dialog.exec()
+        self.dialog.exec()
 
 
 app = QtWidgets.QApplication(sys.argv)
